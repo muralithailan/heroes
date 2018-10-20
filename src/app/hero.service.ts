72db2b9fad803e5452dbc487c7c2253cc0b235e7
murali <murali.thailan@gmail.com>
2018-10-20 21:22:59 +0530

    list of heroes mock server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import {HttpClient,HttpHeaders} from '@angular/common/http';
 import {Hero} from './hero';
 import {Observable,of} from 'rxjs';
 import {MessageService} from './message.service';
+import {catchError,map,tap} from 'rxjs/operators'
 
 
 @Injectable({
@@ -13,18 +14,36 @@ export class HeroService {
   private heroesUrl = 'api/heroes';
   constructor(private httpClient : HttpClient,private messageService:MessageService) { }
 
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+   
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+   
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+   
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
   getHeroes():Observable<Hero[]>{
     
-    return this.httpClient.get<Hero[]>(this.heroesUrl);
+    return this.httpClient.get<Hero[]>(this.heroesUrl).pipe(
+      catchError(this.handleError('getHeroes',[]))
+    );
   }
 
-  getHero(id):Observable<Hero>{
+  getHero(id:number):Observable<Hero>{
+    const url = `${this.heroesUrl}/${id}`;
     this.messageService.addMessage(`Heroes fetched.. for id =${id}`);
-    return of();
+    return this.httpClient.get<Hero>(url).pipe(
+      catchError(this.handleError<Hero>('get Hero id = `$id`'))
+    )
   }
 
-  private log(message: string) {
-    sr:String;
+  private log(message: string) {    
     this.messageService.addMessage(`HeroService: ${message}`);
   }
 }
